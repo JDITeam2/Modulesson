b2278f23e58c136c4f3b75804e92ca577fb4100c
NAM <hbkt1857@gmail.com>
2024-01-23 23:40:47 +0900

    #0 프로젝트 시작
    프로젝트 초기화
    * 스프링 웹
    * 엑츄레이터
    * 데브툴즈
    * 롬복

---
+++ Modulesson/src/main/java/com/jditeam2/modulesson/ModulessonApplication.java
@@ -0,0 +1,12 @@
+package com.jditeam2.modulesson;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModulessonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ModulessonApplication.class, args);
+    }
+}
