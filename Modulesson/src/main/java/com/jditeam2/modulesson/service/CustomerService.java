8ced9f69c18045e3793cfd183fd19917c22b7d5c
NAM <hbkt1857@gmail.com>
2024-02-16 00:50:13 +0900

    #2 - 클래스 리팩토링 및 config 추가 설정
    - Security @AuthenticationPrincipal 어노테이션 사용중 userDetails 사용시 User 네이밍 충돌, User -> Customer로 변경 진행
    
    - 로그인/로그아웃 config 구현

---
+++ Modulesson/src/main/java/com/jditeam2/modulesson/service/CustomerService.java
@@ -0,0 +1,58 @@
+package com.jditeam2.modulesson.service;
+
+import com.jditeam2.modulesson.repository.ExpertRepository;
+import org.springframework.security.core.userdetails.User;
+import com.jditeam2.modulesson.domain.Customer;
+import com.jditeam2.modulesson.repository.CustomerRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Transactional
+@Service
+public class CustomerService implements UserDetailsService {
+    private final CustomerRepository customerRepository;
+    private final ExpertRepository expertRepository;
+
+    public Customer saveUser(Customer customer) {
+        validateDuplicateUser(customer);
+
+        return customerRepository.save(customer);
+    }
+
+    private void validateDuplicateUser(Customer customer) {
+        Customer findCustomer = customerRepository.findByEmail(customer.getEmail());
+        if (findCustomer != null) {
+            throw new IllegalArgumentException("이미 가입된 회원입니다.");
+        }
+    }
+
+    public void updateUserInfo(String userName, String nickname, String email, String phoneNumber) {
+        Customer customer = customerRepository.findByEmail(email);
+        if (customer != null) {
+            customer.setUserName(userName);
+            customer.setNickname(nickname);
+            customer.setEmail(email);
+            customer.setPhone(phoneNumber);
+            customerRepository.save(customer);
+        }
+    }
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        Customer customer = customerRepository.findByEmail(email);
+
+        if (customer == null) {
+            throw new UsernameNotFoundException(email);
+        }
+
+        return User
+                .builder()
+                .username(customer.getEmail())
+                .password(customer.getPassword())
+                .build();
+    }
+}
--- Modulesson/src/main/java/com/jditeam2/modulesson/service/ExpertService.java
+++ Modulesson/src/main/java/com/jditeam2/modulesson/service/ExpertService.java
@@ -1,15 +1,20 @@
 package com.jditeam2.modulesson.service;
 
+import com.jditeam2.modulesson.domain.Customer;
 import com.jditeam2.modulesson.domain.Expert;
 import com.jditeam2.modulesson.repository.ExpertRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @RequiredArgsConstructor
 @Transactional
 @Service
-public class ExpertService {
+public class ExpertService implements UserDetailsService {
     private final ExpertRepository expertRepository;
 
     public Expert saveExpert(Expert expert) {
@@ -26,7 +31,7 @@ public class ExpertService {
     }
 
     public void updateExpertInfo(String expertName, String nickname, String email, String phone, String introduction) {
-        Expert expert = expertRepository.findByExpertName(expertName);
+        Expert expert = expertRepository.findByEmail(email);
         if (expert != null) {
             expert.setExpertName(expertName);
             expert.setNickname(nickname);
@@ -35,5 +40,20 @@ public class ExpertService {
             expert.setIntroduction(introduction);
             expertRepository.save(expert);
         }
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        Expert expert = expertRepository.findByEmail(email);
+
+        if (expert == null) {
+            throw new UsernameNotFoundException(email);
+        }
+
+        return User
+                .builder()
+                .username(expert.getEmail())
+                .password(expert.getPassword())
+                .build();
     }
 }
